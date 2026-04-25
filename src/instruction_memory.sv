b8e5ecf03a746d0b61ba4e2472501656705f847b
ArjunG12 <46287498+ArjunG12@users.noreply.github.com>
2026-04-26 02:59:57 +0530

    Update instruction_memory.sv

--- src/instruction_memory.sv
+++ src/instruction_memory.sv
@@ -1,36 +1,38 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
+// Company:
+// Engineer:
+//
 // Create Date: 03.03.2026 15:08:11
-// Design Name: 
+// Design Name:
 // Module Name: instruction_memory
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
+// Project Name:
+// Target Devices:
+// Tool Versions:
+// Description:
+//
+// Dependencies:
+//
 // Revision:
 // Revision 0.01 - File Created
 // Additional Comments:
-// 
+//
 //////////////////////////////////////////////////////////////////////////////////
 
-
-module instruction_memory (
+module instruction_memory #(
+    parameter INIT_FILE = "C:/Users/goela/risc_5/risc_5.sim/program.hex"
+) (
     input  logic [31:0] PC,
     output logic [31:0] RD
 );
 
-    logic [31:0] instr_mem [1023:0]; 
+    logic [31:0] instr_mem [1023:0];
+    logic [9:0]  word_addr;
 
     initial begin
-            $readmemh("C:/Users/goela/risc_5/risc_5.sim/bge_test.hex", instr_mem); // ← replaces the dummy loop
-        end
-    
-    
-    assign RD = instr_mem[PC[9:2]];  // byte 0 → LSB
+        $readmemh(INIT_FILE, instr_mem);
+    end
+
+    assign word_addr = PC[11:2];
+    assign RD = instr_mem[word_addr];  // byte 0 -> LSB
 endmodule
